c518ebc8141c90e98a86e4588523a94d1bdd9c1c
Dimitri Depardieu <dimitridepardieu@gmail.com>
2025-04-13 15:22:57 +0200

    Always display cursor as a block

--- init.lua
+++ init.lua
@@ -104,6 +104,9 @@ vim.opt.number = true
 --  Experiment for yourself to see if you like it!
 vim.opt.relativenumber = true
 
+-- Display cursor as a block in all modes
+vim.opt.guicursor = ''
+
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
 
